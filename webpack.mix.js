0c339d330cda93dfeea7438e4082d23ba7f0ed3f
Marcel Goraus <marcelphilippgoraus@gmail.com>
2025-03-26 22:29:51 +0100

    init

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require('laravel-mix');
+
+mix
+  .js('src/js/app.js', 'assets')
+  .postCss('src/css/app.css', 'assets', [require('@tailwindcss/postcss')])
+  .options({
+    processCssUrls: false,
+  });
